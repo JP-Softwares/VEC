64a641caf66293055a74bd647aa208a780c5ace1
Andre-Luiz01 <andreff.net@gmail.com>
2023-06-06 19:57:33 -0300

    teste

---
+++ src/main/java/com/jp/modelos/Gastos.java
@@ -0,0 +1,12 @@
+package com.jp.modelos;
+import java.util.Date;
+
+public class Gastos {
+
+    private int id = 0;
+    private String descricao = "";
+    private double valor = 0.0;
+    private Date data = null;
+
+    
+}
